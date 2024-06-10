b010e23d65c3364b3c8fd51061fd2056caf7ac84
lukemcook <95878598+lukemcook@users.noreply.github.com>
2024-06-10 09:27:37 +0900

    Update pyflation.py

--- pyflation.py
+++ pyflation.py
@@ -2,7 +2,7 @@ import cpi
 
 def inflation_adjustment():
     """
-    Adjusts the initial amount for inflation to the specified comparison year.
+    Adjusts the initial amount ($USD) for inflation to the specified comparison year.
 
     Args:
         initial_amount (int): The amount to adjust for inflation.
@@ -13,7 +13,7 @@ def inflation_adjustment():
 
     Prints the adjusted amount to the console.
     """
-    initial_amount = int(input("Enter the amount you would like to adjust for inflation: $"))
+    initial_amount = int(input("Enter the amount (USD) you would like to adjust for inflation: $"))
     comparison_year = int(input("Enter the year you would like to adjust the amount to: "))
     inflated_amount = cpi.inflate(initial_amount, comparison_year)
     print(f"In the year {comparison_year}, ${initial_amount} was equivalent to ${inflated_amount:.2f}.")
